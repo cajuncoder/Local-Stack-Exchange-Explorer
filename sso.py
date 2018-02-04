f191cc598d747e2831603a77b236eb815e355d9d
Luke Dupont <lukesdupont@gmail.com>
2018-02-04 15:43:06 +0900

    Reorganized folders and refined search behavior

--- sso.py
+++ sso.py
@@ -1,19 +1,3 @@
-# StackSearchOffline (sso)
-# Created by Luke Samuel Dupont, 01/29/2018
-# Contributors:
-
-# Purpose:
-# View StackExchange / StackOverflow questions and answers offline without having
-# to download the entire stackoverflow dump files! This is meant to be a simple
-# csv searcher / viewer for csv files created for specific StackOverflow / 
-# StackExchange tags: ie, 'top 1000 python questions sorted by view count',
-# and search them offline. This takes very little hard disk space, is quicker than
-# searching online, and doesn't require internet access.
-
-# Notes:
-# This is meant to be an extremely simple, portable CLI program.
-# This program is open-source. Contributions are welcome!
-
 #TODO: Search for each word individually
 #TODO: Add CLI parameter for csv file to Search
 #TODO:     > Different DISPLAY_PROGRAM_CMD defaults for different OS's? (ie, w3m is not available on Windows)
@@ -34,8 +18,9 @@ DISPLAY_PROGRAM_CMD = "w3m -dump temp.html | less"
 #Uncmment for html displayed in chromium
 #DISPLAY_PROGRAM_CMD = "chromium temp.html &"
 #filename = 'csv/python-top-1000.csv'
+MAX_RESULTS = 24
 
-csvs = glob.glob('csv/*.csv')
+csvs = glob.glob('*-csv/*.csv')
 print 'Please select a csv file to search:'
 for i in range(len(csvs)):
     print i, ': ', csvs[i] 
@@ -65,14 +50,22 @@ def search():
     query_words = query.split(' ')
 
     #results = []
-    for i in range(0, len(thread)):
-        matches = 0
-        for w in range(0, len(query_words)):
-            if query_words[w].lower() in thread[i][0].lower():
-                matches = matches + 1
-            if matches == len(query_words):
-                #results.add(i)
-                print i, ': ', thread[i][0]
+    nofresults = 0
+    for i in range(1, len(thread)):
+        if query == "":
+            print i, ': ', thread[i][0]
+            nofresults = nofresults + 1
+            if nofresults > MAX_RESULTS:
+                break
+        else:
+            matches = 0
+            for w in range(0, len(query_words)):
+                if query_words[w].lower() in thread[i][0].lower():
+                    matches = matches + 1
+                if matches == len(query_words):
+                    #results.add(i)
+                    nofresults = nofresults + 1
+                    print i, ': ', thread[i][0]
 
 def choose_thread():
     print 'Choose thread to view:'
